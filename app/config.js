8ce5e487a630e31a67b85e01d5ce1f788485f994
Martin Wake <martin.wake@digital.homeoffice.govuk>
2017-05-18 17:16:08 +0100

    Updating content - needs checking and 2i

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Sponsorship fee calculator',
+  serviceName: 'Check if you\'re a large or a small sponsor',
 
   // Default port that prototype runs on
   port: '3000',
